ff3977605a65bcd15fd3ebc1e6cbf7f42bede16f
Yiye Chen <2019cyy@gmail.com>
2022-08-22 23:35:42 -0400

    Make it a package. Modify the module path in checkpoint config

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='contactGraspNet',
+      version='1.0',
+      description='The contact graspnet package',
+      author='Yiye Chen',
+      packages=['contactGraspNet'],
+      )
